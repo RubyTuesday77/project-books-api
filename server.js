a91cc8711c2399e51dcc96d9eb0507692b7b550a
RubyTuesday77 <105758468+RubyTuesday77@users.noreply.github.com>
2022-10-14 19:52:39 -0400

    added controller and model

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-// DEPENDENCIES/CONFIGURATION:
+// DEPENDENCIES
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+
+
+// CONFIGURATION/MIDDLEWARE:
+require('dotenv').config()
+const PORT = process.env.PORT
 app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
 
 
+// MONGOOSE:
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => { 
+    console.log('connected to mongo on: ', process.env.MONGO_URI)
+})
 
 
 // ROUTES:
@@ -13,8 +24,11 @@ app.get('/', (req, res) => {
 })
 
 // Books:
-
+const booksController = require('./controllers/books_controller.js')
+app.use('/books', booksController)
 
 
 // LISTEN:
-app.listen(3000)
+app.listen(PORT, () => {
+    console.log('Greetings! From port: ', PORT)
+})
